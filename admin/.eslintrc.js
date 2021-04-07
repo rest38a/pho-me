d8b40c86ddcaab135066c59c3168b7406770b889
nobasksio <a420673@yandex.ru>
2021-04-07 17:29:39 +0800

    iiko menu manage

--- admin/.eslintrc.js
+++ admin/.eslintrc.js
@@ -48,9 +48,7 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
     'prefer-promise-reject-errors': 'off',
-    'linebreak-style': '0',
-
-
+    'linebreak-style': 0,
 
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
